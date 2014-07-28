872890bad7a6781a0fc0734b5a23563744397815
Chris <chriskellendonk@gmail.com>
2014-07-27 20:35:51 -0400

    Button

---
+++ Tipster/Tipster/CircleButton.swift
@@ -0,0 +1,14 @@
+//
+//  CircleButton.swift
+//  Tipster
+//
+//  Created by Chris Kellendonk on 2014-07-27.
+//  Copyright (c) 2014 Chris Kellendonk. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CircleButton: UIButton {
+    
+}
